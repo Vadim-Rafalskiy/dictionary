b95e9bd655ec5de33b1d24ee574a33f071ad7ebf
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2024-09-07 17:07:08 +0200

    Reapply "create conect to mongoose"
    This reverts commit 36e662cf5f821a0938ded2a84a20c289764ce70a.

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,21 @@
+const mongoose = require('mongoose');
+
 const app = require('./app');
 
 const PORT = 3001;
-app.listen(PORT, () => {
-    console.log(`server is runing on port ${PORT}`);
-});
+
+const DB_HOST =
+    'mongodb+srv://Vadym:p2CU7ihzdnowNIHH@cluster0.ojufj.mongodb.net/Dictionary?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose
+    .connect(DB_HOST)
+    .then(() =>
+        app.listen(PORT, () => {
+            console.log(`server is runing on port ${PORT}`);
+        })
+    )
+    .catch((err) => console.error(err.message));
+
+// app.listen(PORT, () => {
+//     console.log(`server is runing on port ${PORT}`);
+// });
